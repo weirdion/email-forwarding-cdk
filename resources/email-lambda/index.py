e29744a02f01d9a59ac0c54f5950647c91c07d73
Ankit Sadana <sadana.ankit@gmail.com>
2022-04-07 21:53:06 -0400

    Added dynamic handling for email forwarding

--- resources/email-lambda/index.py
+++ resources/email-lambda/index.py
@@ -16,25 +16,28 @@ import os
 import boto3
 import email
 import re
+import html
 from botocore.exceptions import ClientError
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
+from email.mime.image import MIMEImage
 
 from aws_lambda_powertools import Logger
 
-log = Logger(service="EmaiForwardLamba")
+log = Logger(service="EmailForwardLambda")
 
-region = os.environ['Region']
+region = os.environ["REGION"]
+incoming_email_bucket = os.environ["BUCKET_NAME"]
+incoming_email_prefix = os.environ["EMAIL_S3_PREFIX"]
+send_from_address = os.environ["FORWARDING_SENDER"]
 
 
 def get_message_from_s3(message_id):
 
-    incoming_email_bucket = os.environ['MailS3Bucket']
-    incoming_email_prefix = os.environ['MailS3Prefix']
 
     if incoming_email_prefix:
-        object_path = (incoming_email_prefix + "/" + message_id)
+        object_path = f"{incoming_email_prefix}/{message_id}"
     else:
         object_path = message_id
 
@@ -44,10 +47,12 @@ def get_message_from_s3(message_id):
     client_s3 = boto3.client("s3")
 
     # Get the email object from the S3 bucket.
-    object_s3 = client_s3.get_object(Bucket=incoming_email_bucket,
-        Key=object_path)
+    object_s3 = client_s3.get_object(
+        Bucket=incoming_email_bucket,
+        Key=object_path
+    )
     # Read the content of the message.
-    file = object_s3['Body'].read()
+    file = object_s3["Body"].read()
 
     file_dict = {
         "file": file,
@@ -56,80 +61,129 @@ def get_message_from_s3(message_id):
 
     return file_dict
 
+
 def create_message(file_dict):
 
-    sender = os.environ['MailSender']
-    recipient = os.environ['MailRecipient']
+    stringMsg = file_dict["file"].decode("utf-8")
 
-    separator = ";"
+    # Create a MIME container.
+    msg = MIMEMultipart("alternative")
 
     # Parse the email body.
-    mailobject = email.message_from_string(file_dict['file'].decode('utf-8'))
+    mailobject = email.message_from_string(file_dict["file"].decode("utf-8"))
+    log.info(f"Mail object: {mailobject}")
+
+    # Get original sender, reply-to, recipient, cc and return-path
+    return_path = mailobject["Return-Path"]
+    return_path = return_path.replace("<", "")
+    return_path = return_path.replace(">", "")
+
+    from_address = mailobject["From"]
+    reply_to_address = mailobject["Reply-To"]
+    recipient_address = mailobject["To"]
+    recipient_cc_address = mailobject["Cc"]
+
+    
+    log.info(f"From address: {from_address}")
+    log.info(f"Reply To: {reply_to_address}")
+    log.info(f"Recipient: {recipient_address}, CC: {recipient_cc_address}")
+    log.info(f"Return path: {return_path}")
 
     # Create a new subject line.
-    subject_original = mailobject['Subject']
-    subject = "FW: " + subject_original
+    subject = mailobject["Subject"]
+    log.info(f"Subject: {subject}")
 
-    # The body text of the email.
-    body_text = ("The attached message was received from "
-              + separator.join(mailobject.get_all('From'))
-              + ". This message is archived at " + file_dict['path'])
+    if mailobject.is_multipart():
+
+        index = stringMsg.find("Content-Type: multipart/")
+        stringBody = stringMsg[index:]
+        log.info(f"String body: {stringBody}")
+
+        for part in mailobject.walk():
+            ctype = part.get_content_type()
+            cdispo = str(part.get("Content-Disposition"))
+
+            # case for each common content type
+            if ctype == "text/plain" and "attachment" not in cdispo:
+                bodyPart = MIMEText(part.get_payload(decode=True), "plain", part.get_content_charset())
+                msg.attach(bodyPart)
+
+            if ctype == "text/html" and "attachment" not in cdispo:
+                mt = MIMEText(part.get_payload(decode=True), "html", part.get_content_charset())
+                email.encoders.encode_quopri(mt)
+                del mt["Content-Transfer-Encoding"]
+                mt.add_header("Content-Transfer-Encoding", "quoted-printable")
+                msg.attach(mt)
+
+            if "attachment" in cdispo and "image" in ctype:
+                mi = MIMEImage(part.get_payload(decode=True), ctype.replace("image/", ""))
+                del mi["Content-Type"]
+                del mi["Content-Disposition"]
+                mi.add_header("Content-Type", ctype)
+                mi.add_header("Content-Disposition", cdispo)
+                msg.attach(mi)
+
+            if "attachment" in cdispo and "application" in ctype:
+                ma = MIMEApplication(part.get_payload(decode=True), ctype.replace("application/", ""))
+                del ma["Content-Type"]
+                del ma["Content-Disposition"]
+                ma.add_header("Content-Type", ctype)
+                ma.add_header("Content-Disposition", cdispo)
+                msg.attach(ma)
+
+
+    # not multipart - i.e. plain text, no attachments, keeping fingers crossed
+    else:
+        body = MIMEText(mailobject.get_payload(decode=True), "UTF-8")
+        msg.attach(body)
 
     # The file name to use for the attached message. Uses regex to remove all
     # non-alphanumeric characters, and appends a file extension.
-    filename = re.sub('[^0-9a-zA-Z]+', '_', subject_original) + ".eml"
-
-    # Create a MIME container.
-    msg = MIMEMultipart()
-    # Create a MIME text part.
-    text_part = MIMEText(body_text, _subtype="html")
-    # Attach the text part to the MIME message.
-    msg.attach(text_part)
+    filename = re.sub("[^0-9a-zA-Z]+", "_", subject)
 
     # Add subject, from and to lines.
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = recipient
+    msg["Subject"] = subject
+    msg["From"] = from_address
+    msg["To"] = recipient_address
+    msg["Cc"] = recipient_cc_address
+    msg["reply-to"] = reply_to_address if reply_to_address else from_address
 
     # Create a new MIME object.
     att = MIMEApplication(file_dict["file"], filename)
-    att.add_header("Content-Disposition", 'attachment', filename=filename)
+    att.add_header("Content-Disposition", "attachment", filename=filename)
 
     # Attach the file object to the message.
     msg.attach(att)
-
     message = {
-        "Source": sender,
-        "Destinations": recipient,
+        "Source": send_from_address,
+        "Destinations": recipient_address,
         "Data": msg.as_string()
     }
-
     return message
 
+
 def send_email(message):
-    aws_region = os.environ['Region']
-
 # Create a new SES client.
-    client_ses = boto3.client('ses', region)
-
+    client_ses = boto3.client("ses", region)
     # Send the email.
     try:
         #Provide the contents of the email.
         response = client_ses.send_raw_email(
-            Source=message['Source'],
+            Source=message["Source"],
             Destinations=[
-                message['Destinations']
+                message["Destinations"]
             ],
             RawMessage={
-                'Data':message['Data']
+                "Data":message["Data"]
             }
         )
 
     # Display an error if something goes wrong.
     except ClientError as e:
-        output = e.response['Error']['Message']
+        log.info("send email ClientError Exception")
+        output = e.response["Error"]["Message"]
     else:
-        output = "Email sent! Message ID: " + response['MessageId']
+        output = f"Email sent! Message ID: {response["MessageId"]}"
 
     return output
 
@@ -138,8 +192,8 @@ def send_email(message):
 def handler(event, context):
     # Get the unique ID of the message. This corresponds to the name of the file
     # in S3.
-    message_id = event['Records'][0]['ses']['mail']['messageId']
-    print(f"Received message ID {message_id}")
+    message_id = event["Records"][0]["ses"]["mail"]["messageId"]
+    log.info(f"Received message ID {message_id}")
 
     # Retrieve the file from the S3 bucket.
     file_dict = get_message_from_s3(message_id)
@@ -147,6 +201,6 @@ def handler(event, context):
     # Create the message.
     message = create_message(file_dict)
 
-    # Send the email and print the result.
+    # Send the email and log.info the result.
     result = send_email(message)
-    print(result)
+    log.info(f"Result: {result}")
