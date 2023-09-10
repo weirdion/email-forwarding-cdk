bf8ab1219a407960a3b15091ab6f79beaa06c687
Ankit Sadana <sadana.ankit@gmail.com>
2023-09-09 22:38:10 -0400

    Update lambdas

--- resources/email-lambda/email_handler.py
+++ resources/email-lambda/email_handler.py
@@ -1,3 +1,5 @@
+# Original code taken lovingly from AWS Blog:
+# https://aws.amazon.com/blogs/messaging-and-targeting/forward-incoming-email-to-an-external-destination/
 # Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # This file is licensed under the Apache License, Version 2.0 (the "License").
@@ -10,11 +12,8 @@
 # OF ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-# Original code taken lovingly from AWS Blog: https://aws.amazon.com/blogs/messaging-and-targeting/forward-incoming-email-to-an-external-destination/
-
 import email
 import os
-import re
 from email.mime.application import MIMEApplication
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
@@ -25,7 +24,7 @@ import boto3
 from aws_lambda_powertools import Logger
 from botocore.exceptions import ClientError
 
-from ssm_handler import SSMHandler
+from ssm_handler import SSMHandler, EmailForwardMap
 
 region = os.environ["REGION"]
 incoming_email_bucket = os.environ["BUCKET_NAME"]
@@ -35,99 +34,100 @@ log = Logger(child=True)
 
 
 def get_message_from_s3(message_id) -> Dict:
-
-
     if incoming_email_prefix:
         object_path = f"{incoming_email_prefix}/{message_id}"
     else:
         object_path = message_id
 
-    object_http_path = (f"http://s3.console.aws.amazon.com/s3/object/{incoming_email_bucket}/{object_path}?region={region}")
+    object_http_path = f"https://s3.console.aws.amazon.com/s3/object/{incoming_email_bucket}/{object_path}?region={region}"
 
     # Create a new S3 client.
     client_s3 = boto3.client("s3")
 
     # Get the email object from the S3 bucket.
-    object_s3 = client_s3.get_object(
-        Bucket=incoming_email_bucket,
-        Key=object_path
-    )
+    object_s3 = client_s3.get_object(Bucket=incoming_email_bucket, Key=object_path)
     # Read the content of the message.
     file = object_s3["Body"].read()
 
-    file_dict = {
-        "file": file,
-        "path": object_http_path
-    }
+    file_dict = {"file": file, "path": object_http_path}
 
     return file_dict
 
 
 def create_message(file_dict: Dict, ssm_handler: SSMHandler):
-
-    stringMsg = file_dict["file"].decode("utf-8")
+    string_msg = file_dict["file"].decode("utf-8")
 
     # Create a MIME container.
     msg = MIMEMultipart("alternative")
 
     # Parse the email body.
-    mailobject = email.message_from_string(file_dict["file"].decode("utf-8"))
-    log.info(f"Mail object: {mailobject}")
+    mail_object = email.message_from_string(file_dict["file"].decode("utf-8"))
+    log.info(f"Mail object: {mail_object}")
 
     # Get original sender, reply-to, recipient, cc and return-path
-    return_path = mailobject["Return-Path"]
+    return_path = mail_object["Return-Path"]
     return_path = return_path.replace("<", "")
     return_path = return_path.replace(">", "")
 
-    from_address = mailobject["From"]
-    reply_to_address = mailobject["Reply-To"]
-    recipient_address = mailobject["To"]
-    recipient_cc_address = mailobject["Cc"]
+    from_address = mail_object["From"]
+    reply_to_address = mail_object["Reply-To"]
+    recipient_address = mail_object["To"]
+    recipient_cc_address = mail_object["Cc"]
 
     # Create a new subject line.
-    subject = mailobject["Subject"]
+    subject = mail_object["Subject"]
     log.info(f"Subject: {subject}")
 
     # Match receiving address with alias email map
-    email_fwd_map: ssm_handler.EmailForwardMap = ssm_handler.get_recipients_for_address(recipient_address)
+    email_fwd_map: EmailForwardMap = ssm_handler.get_recipients_for_address(
+        recipient_address
+    )
     if not email_fwd_map:
         raise ValueError(f"There was no match for to-address: {recipient_address}")
 
     fwd_recipients = email_fwd_map.get_recipients()
-    log.info("Summary of email", extra={
-        "Subject": subject,
-        "From": from_address,
-        "To": recipient_address,
-        "CC": recipient_cc_address,
-        "Return-path": return_path,
-        "Reply-To": reply_to_address,
-        "Sending-To": fwd_recipients
-    })
+    log.info(
+        "Summary of email",
+        extra={
+            "Subject": subject,
+            "From": from_address,
+            "To": recipient_address,
+            "CC": recipient_cc_address,
+            "Return-path": return_path,
+            "Reply-To": reply_to_address,
+            "Sending-To": fwd_recipients,
+        },
+    )
 
-    if mailobject.is_multipart():
+    if mail_object.is_multipart():
+        index = string_msg.find("Content-Type: multipart/")
+        string_body = string_msg[index:]
+        log.info(f"String body: {string_body}")
 
-        index = stringMsg.find("Content-Type: multipart/")
-        stringBody = stringMsg[index:]
-        log.info(f"String body: {stringBody}")
-
-        for part in mailobject.walk():
+        for part in mail_object.walk():
             ctype = part.get_content_type()
             cdispo = str(part.get("Content-Disposition"))
 
             # case for each common content type
             if ctype == "text/plain" and "attachment" not in cdispo:
-                bodyPart = MIMEText(part.get_payload(decode=True), "plain", part.get_content_charset())
-                msg.attach(bodyPart)
+                body_part = MIMEText(
+                    part.get_payload(decode=True), "plain", part.get_content_charset()
+                )
+                msg.attach(body_part)
 
             if ctype == "text/html" and "attachment" not in cdispo:
-                mt = MIMEText(part.get_payload(decode=True), "html", part.get_content_charset())
+                mt = MIMEText(
+                    part.get_payload(decode=True), "html", part.get_content_charset()
+                )
                 email.encoders.encode_quopri(mt)
                 del mt["Content-Transfer-Encoding"]
                 mt.add_header("Content-Transfer-Encoding", "quoted-printable")
                 msg.attach(mt)
 
             if "attachment" in cdispo and "image" in ctype:
-                mi = MIMEImage(part.get_payload(decode=True), ctype.replace("image/", ""))
+                mi = MIMEImage(
+                    part.get_payload(decode=True), ctype.replace("image/", "")
+                )
                 del mi["Content-Type"]
                 del mi["Content-Disposition"]
                 mi.add_header("Content-Type", ctype)
@@ -135,17 +135,18 @@ def create_message(file_dict: Dict, ssm_handler: SSMHandler):
                 msg.attach(mi)
 
             if "attachment" in cdispo and "application" in ctype:
-                ma = MIMEApplication(part.get_payload(decode=True), ctype.replace("application/", ""))
+                ma = MIMEApplication(
+                    part.get_payload(decode=True), ctype.replace("application/", "")
+                )
                 del ma["Content-Type"]
                 del ma["Content-Disposition"]
                 ma.add_header("Content-Type", ctype)
                 ma.add_header("Content-Disposition", cdispo)
                 msg.attach(ma)
 
-
     # not multipart - i.e. plain text, no attachments, keeping fingers crossed
     else:
-        body = MIMEText(mailobject.get_payload(decode=True), "UTF-8")
+        body = MIMEText(mail_object.get_payload(decode=True), "UTF-8")
         msg.attach(body)
 
     # The file name to use for the attached message. Uses regex to remove all
@@ -168,7 +169,7 @@ def create_message(file_dict: Dict, ssm_handler: SSMHandler):
     message = {
         "Source": email_fwd_map.from_sender,
         "Destinations": email_fwd_map.recipients,
-        "Data": msg.as_string()
+        "Data": msg.as_string(),
     }
     return message
 
@@ -178,13 +179,11 @@ def send_email(message):
     client_ses = boto3.client("ses", region)
     # Send the email.
     try:
-        #Provide the contents of the email.
+        # Provide the contents of the email.
         response = client_ses.send_raw_email(
             Source=message["Source"],
             Destinations=message["Destinations"],
-            RawMessage={
-                "Data":message["Data"]
-            }
+            RawMessage={"Data": message["Data"]},
         )
 
     # Display an error if something goes wrong.
--- resources/email-lambda/ssm_handler.py
+++ resources/email-lambda/ssm_handler.py
@@ -1,13 +1,11 @@
-from ast import alias
-import re
-from dataclasses import dataclass
 import json
 import os
+import re
+from dataclasses import dataclass
 from typing import List
+
 import boto3
-
 from aws_lambda_powertools import Logger
-
 
 EMAIL_MAP_SSM = os.environ["EMAIL_MAP_SSM"]
 EMAIL_FORMAT = r"([a-zA-Z0-9_]+)[a-zA-Z0-9_.+-]*@([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
@@ -16,22 +14,13 @@ log = Logger(child=True)
 
 @dataclass
 class EmailForwardMap:
-    # {
-    #     "from_sender": "no-reply@example.com",
-    #     "alias": "a@example.com",
-    #     "recipients": [
-    #         "a@myExistingEmail.com",
-    #         "b@myExistingEmail.com"
-    #     ],
-    #     "email_prefix": "FW: "
-    # }
     from_sender: str
     alias: str
     recipients: List[str]
     email_prefix: str
 
     def get_recipients(self):
-        return  ", ".join(self.recipients)
+        return ", ".join(self.recipients)
 
 
 class SSMHandler:
@@ -41,15 +30,15 @@ class SSMHandler:
         self.email_map = response["Parameter"]["Value"]
         email_map_list = json.loads(self.email_map)
         self.email_fw_map: List[EmailForwardMap] = []
-        for map in email_map_list:
-            self.email_fw_map.append(EmailForwardMap(**map))
+        for e_map in email_map_list:
+            self.email_fw_map.append(EmailForwardMap(**e_map))
 
     def get_recipients_for_address(self, to_address: str):
         # -1 to get the last tuple and ignore naming like Ankit <ankit@example.com>
         sanitized_to_address = "@".join(re.findall(EMAIL_FORMAT, to_address)[-1])
 
-        for map in self.email_fw_map:
-            if sanitized_to_address == map.alias:
-                log.info(f"Mapping To address from: {to_address} -> {map.alias} -> {map.recipients}")
-                return map
+        for e_map in self.email_fw_map:
+            if sanitized_to_address == e_map.alias:
+                log.info(f"Mapping To address from: {to_address} -> {e_map.alias} -> {e_map.recipients}")
+                return e_map
         return None
